7e447997821431cec4e0c62913430f2cd00c1b46
doodlecoge@163.com <doodlecoge@163.com>
2015-01-12 11:04:55 +0800

    add dialog plugin, split into separate

---
+++ jquery-dialog.js
@@ -0,0 +1,100 @@
+/**
+ * Created by Administrator on 2015/1/12.
+ */
+
+;(function($){
+    /*=======================================================================*/
+    /* dialog                                                                */
+    /*=======================================================================*/
+    $.fn.plugin('dialog', {
+        _create: function () {
+            this._create_overlay();
+            this._create_wrapper();
+            this._createButtons();
+            this._initEventHandler();
+            this.show();
+        },
+        _createButtons: function () {
+            if (!this.options.buttons) return;
+
+            var buttonSet = $('<div>')
+                .addClass('dlg_btn_set')
+                .appendTo(this.wrapper);
+
+            var that = this;
+
+            $.each(this.options.buttons, function (name, fn) {
+                var button = $('<button>')
+                    .html(name)
+                    .appendTo(buttonSet);
+
+                that._on(button, {
+                    click: function (e) {
+                        fn(e, this);
+                        this._close();
+                    }
+                });
+            });
+        },
+        _create_wrapper: function () {
+            this.wrapper = $('<div class="dlg">').hide()
+                .appendTo(document.body);
+
+            var title = $('<div class="dlg_title">')
+                .appendTo(this.wrapper)
+                .html(this.options.title || '');
+
+
+            var close_btn = $('<a>')
+                .addClass('dlg_close_btn')
+                .attr('href', 'javascript:;')
+                .appendTo(title);
+
+
+            $('<i>')
+                .addClass('fa')
+                .addClass('fa-times')
+                .appendTo(close_btn);
+
+
+            var content = $('<div>')
+                .addClass('dlg_content')
+                .appendTo(this.wrapper);
+
+            this.element.appendTo(content);
+        },
+        _create_overlay: function () {
+            var overlay = $('.global_overlay');
+            if (overlay.length == 0) {
+                overlay = $('<div>')
+                    .addClass('global_overlay')
+                    .addClass('t30')
+                    .appendTo(document.body);
+            }
+            this.overlay = overlay.hide();
+        },
+        _initEventHandler: function () {
+            var btn = this.wrapper.find('.dlg_close_btn');
+
+            this._on(btn, {
+                click: function () {
+                    this._close();
+                }
+            });
+        },
+        show: function () {
+            this.overlay.show();
+            this.wrapper.show();
+            var w = this.wrapper.width();
+            var h = this.wrapper.height();
+            this.wrapper.css('left', '50%');
+            this.wrapper.css('top', '50%');
+            this.wrapper.css('margin-left', (-w / 2) + 'px');
+            this.wrapper.css('margin-top', (-h / 2) + 'px');
+        },
+        _close: function () {
+            this.overlay.hide();
+            this.wrapper.hide();
+        }
+    });
+})(jQuery);
---
+++ jquery-plugin.js
@@ -0,0 +1,85 @@
+/**
+ * Created by huaichao on 2015/1/12.
+ */
+;(function ($) {
+    $.fn.plugin =function(name, prototype) {
+        var constructor = function (options, element) {
+            this.options = options || {};
+            this.element = $(element);
+            this.localExtensions = {};
+            if (this._create) this._create();
+            if (this._init) this._init();
+        };
+        constructor.prototype = $.extend({
+            _on: function (element, handlers) {
+                var _this = this;
+                $.each(handlers, function (event, handler) {
+                    element.bind(event, function () {
+                        handler.apply(_this, arguments);
+                    });
+                });
+            },
+            globalExtensions: {},
+            extendGlobal: function (name, extObj) {
+                if (this.globalExtensions[name])
+                    $.error('global extension with same name already exists');
+                this.globalExtensions[name] = extObj;
+            },
+            extendLocal: function (name, extObj) {
+                if (this.localExtensions[name])
+                    $.error('local extension with same name already exists');
+                this.localExtensions[name] = extObj;
+            }
+        }, prototype);
+
+
+        $.fn[name] = $.fn[name] || function (options) {
+
+            var isMethodCall = typeof options === 'string';
+            var args = Array.prototype.slice.call(arguments, 1);
+            var ret = undefined;
+            this.each(function (i) {
+
+                var r = undefined;
+                var ins = $.data(this, name);
+                if (isMethodCall) {
+                    if (!ins) $.error('not initialized yet');
+
+                    var called = false;
+
+                    $.each(ins.localExtensions, function (name, ext) {
+                        if (!$.isFunction(ext[options])) return;
+                        r = ext[options].apply(ins, args);
+                        called = true;
+                        return false;
+                    });
+
+                    if (called == false) {
+                        $.each(ins.globalExtensions, function (name, ext) {
+                            if (!$.isFunction(ext[options])) return;
+                            r = ext[options].apply(ins, args);
+                            called = true;
+                            return false;
+                        });
+                    }
+
+                    if (called == false) {
+                        if (!$.isFunction(ins[options]) || options.charAt(0) === '_')
+                            $.error('no such method: ' + options);
+                        r = ins[options].apply(ins, args);
+                    }
+                } else {
+                    if (ins) {
+                        if (ins._init) r = ins._init();
+                    } else {
+                        ins = new constructor(options, this);
+                        r = ins;
+                        $.data(this, name, ins);
+                    }
+                }
+                if (i == 0) ret = r;
+            });
+            return ret;
+        }
+    }
+})(jQuery);
