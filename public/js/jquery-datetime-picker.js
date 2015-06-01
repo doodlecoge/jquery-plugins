29d6f0f4370113fbf40aabc5db94833d73f0881d
doodlecoge@163.com <doodlecoge@163.com>
2015-06-01 17:46:24 +0800

    use hammer.js

--- public/js/jquery-datetime-picker.js
+++ public/js/jquery-datetime-picker.js
@@ -31,64 +31,63 @@
             }
         },
         _init: function () {
-            this._on(this.touchpad, {
-                'mousedown': function (e) {
-                    this.moving = true;
-                    this.y = e.pageY;
-                    this.ts = new Date();
-                    this.top = -this.spinner.height();
-                    this.ch = this.spinner.height() / 3;
-                    return false;
-                },
-                'mousemove': function (e) {
-                    if (!this.moving)return;
-                    var dy = e.pageY - this.y;
-                    this.cards.css('top', (this.top + dy) + 'px');
-                },
-                'mouseup': this._stopping,
-                'mouseout': this._stopping
-            })
+            var that = this;
+            var mc = new Hammer.Manager(this.touchpad.get(0));
+            mc.add(
+                new Hammer.Pan({
+                    direction: Hammer.DIRECTION_VERTICAL,
+                    threshold: 0
+                })
+            );
+            mc.on("panstart", function (e) {
+                that.top = -that.spinner.height();
+                that.ch = that.spinner.height() / 3;
+            });
+            mc.on("panmove", function (e) {
+                that.cards.css('top', (that.top + e.deltaY) + 'px');
+            });
+
+            mc.on("panend", function (e) {
+                that._stopping(e.deltaY, e.velocity);
+            });
         },
         _updateVal: function () {
             var that = this;
+            this.cards.css('top', '-33.3333%');
             this.cards.find('.card').each(function (i, card) {
                 $(card).html(that.val + i - 4);
             });
         },
-        _stopping: function (e) {
-            if (!this.moving) return;
-            this.moving = false;
+        _stopping: function (deltaY, velocity) {
+            var dir = deltaY > 0 ? 1 : -1,
+                deltaY = Math.abs(deltaY),
+                velocity = Math.abs(velocity);
 
-            var dy = e.pageY - this.y;
-            var dt = new Date() - this.ts;
-            var n = Math.floor((Math.abs(dy) + this.ch / 2 - 1) / this.ch);
-            var lvl = Math.max(0, 4 - dt / 50);
-            lvl = Math.round(lvl * Math.sqrt(lvl)) * n;
-            n += lvl;
-            var speed = 300 / Math.max(1, Math.sqrt(lvl));
-
-
-            console.log(n,speed);
-
-
-            this._move(n, dy > 0 ? 1 : -1, speed);
+            var n = Math.floor((deltaY + this.ch / 2 - 1) / this.ch);
+            n += Math.round(Math.exp(velocity));
+            this._rotateTo(this.val + n * dir);
         },
-        _move: function (n, dir, speed) {
-            var x = n > 3 ? 3 : n;
-            n = n - x;
-            speed = speed || 200;
+        _rotateTo: function (targetVal) {
+            if (targetVal == this.val) return;
 
-            var easing = n == 0 ? 'swing' : 'linear';
-
+            this.val += targetVal > this.val ? 1 : -1;
+            this._updateVal();
             var that = this;
-            this.cards.animate({
-                top: (this.top + x * this.ch * dir) + 'px'
-            }, speed, easing, function () {
-                that.val += x * dir * -1;
-                that.cards.css('top', '-100%');
-                that._updateVal();
-                if (n > 0) that._move(n, dir, speed);
-            });
+            setTimeout(function () {
+                that._rotateTo(targetVal);
+            }, 200);
+
+            //var easing = n == 0 ? 'swing' : 'linear';
+            //
+            //var that = this;
+            //this.cards.animate({
+            //    top: (this.top + x * this.ch * dir) + 'px'
+            //}, speed, easing, function () {
+            //    that.val += x * dir * -1;
+            //    that.cards.css('top', '-100%');
+            //    that._updateVal();
+            //    if (n > 0) that._rotateTo(n, dir, speed);
+            //});
         }
     });
     //$.widget("ui.datetimePicker", {
