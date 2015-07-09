aa5b2871f89ca0897c9711bd5edd16948e5cdc92
小王大超 <hchwang001@163.com>
2015-07-09 13:09:36 +0800

    use int value for scrollTop... to avoid shaking

--- public/js/jquery.scrollbar.js
+++ public/js/jquery.scrollbar.js
@@ -3,7 +3,7 @@
 
     $.widget("ui.ScrollBar", {
         _create: function () {
-            this.element.css('overflow', 'hidden');
+            this.element.addClass('scrollbar-container');
             this.rail = $('<div>')
                 .addClass('scrollbar-y-rail')
                 .appendTo(this.element);
@@ -32,6 +32,7 @@
             this._on(this.bar, {
                 mousedown: function (e) {
                     this.scrolling = true;
+                    this.element.addClass('scrollbar-in-scrolling');
                     startY = e.pageY;
                     startBarTop = this.bar.offset().top - this.rail.offset().top;
                     e.stopPropagation();
@@ -46,6 +47,7 @@
                 },
                 mouseup: function () {
                     this.scrolling = false;
+                    this.element.removeClass('scrollbar-in-scrolling');
                 }
             });
             this._on(this.rail, {
@@ -65,17 +67,23 @@
         _updateByBarTop: function (newBarTop) {
             var barTop = Math.max(0, Math.min(newBarTop, panelHeight - barHeight));
 
-            var newScrollTop = barTop / (panelHeight - barHeight) *
-                (panelScrollHeight - panelHeight);
+            var newScrollTop = parseInt(barTop / (panelHeight - barHeight) *
+            (panelScrollHeight - panelHeight));
 
-            this.bar.css('top', barTop + 'px');
-            this.rail.css('top', newScrollTop + 'px');
+            //this.bar.css('top', barTop + 'px');
+            //this.rail.css('top', newScrollTop + 'px');
+            this.bar.get(0).style.top = barTop + 'px';
+            this.rail.get(0).style.top = newScrollTop + 'px';
             this.element.get(0).scrollTop = newScrollTop;
         },
         _updateByScrollTop: function (newScrollTop) {
-            newScrollTop = Math.max(0, Math.min(newScrollTop, panelScrollHeight - panelHeight));
-            var barTop = newScrollTop * (panelHeight - barHeight) /
-                (panelScrollHeight - panelHeight);
+            newScrollTop = parseInt(
+                Math.max(0, Math.min(newScrollTop, panelScrollHeight - panelHeight))
+            );
+            var barTop = parseInt(
+                newScrollTop * (panelHeight - barHeight) /
+                (panelScrollHeight - panelHeight)
+            );
 
             this.bar.css('top', barTop + 'px');
             this.rail.css('top', newScrollTop + 'px');
